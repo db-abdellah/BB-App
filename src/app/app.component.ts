23e93325f5253bceb5407eb94a50b35c68cd71fa
Dbib Abdellah <adbib@sqli.com>
2021-05-02 23:41:38 +0000

    mobile Navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  public  active:boolean = false;
+  mobileMenu() {
+    const hamburger = document.querySelector('.hamburger');
+    const navMenu = document.querySelector('ul');
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+    this.active = !this.active;
+  }
+  toggleActive(){
+    const hamburger = document.querySelector('.hamburger');
+    const navMenu = document.querySelector('ul');
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+    
+  }
+  onResize(event) {
+    
+    if(event.target.innerWidth>900 && this.active==true){
+      this.mobileMenu();
+    }
+  }
 }
